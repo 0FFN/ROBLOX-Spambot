b5c9bfc70f814369ca6c50fa4b07a8eba853dfb3
arc <78260120+arcryi@users.noreply.github.com>
2021-09-18 20:16:21 +0100

    Add files via upload

---
+++ rospambot.py
@@ -0,0 +1,27 @@
+import pyautogui
+import pydirectinput
+from time import sleep as wait
+forever = 1
+from os import system
+system("title "+"Arc's ROBLOX Spambot")
+
+print(''' 
+▄▄▄        ▄▄▄▄· ▄▄▌        ▐▄• ▄     .▄▄ ·  ▄▄▄· ▄▄▄· • ▌ ▄ ·. ▄▄▄▄·       ▄▄▄▄▄
+▀▄ █·▪     ▐█ ▀█▪██•  ▪      █▌█▌▪    ▐█ ▀. ▐█ ▄█▐█ ▀█ ·██ ▐███▪▐█ ▀█▪▪     •██  
+▐▀▀▄  ▄█▀▄ ▐█▀▀█▄██▪   ▄█▀▄  ·██·     ▄▀▀▀█▄ ██▀·▄█▀▀█ ▐█ ▌▐▌▐█·▐█▀▀█▄ ▄█▀▄  ▐█.▪
+▐█•█▌▐█▌.▐▌██▄▪▐█▐█▌▐▌▐█▌.▐▌▪▐█·█▌    ▐█▄▪▐█▐█▪·•▐█ ▪▐▌██ ██▌▐█▌██▄▪▐█▐█▌.▐▌ ▐█▌·
+.▀  ▀ ▀█▄▀▪·▀▀▀▀ .▀▀▀  ▀█▄▀▪•▀▀ ▀▀     ▀▀▀▀ .▀    ▀  ▀ ▀▀  █▪▀▀▀·▀▀▀▀  ▀█▄▀▪ ▀▀▀ 
+Made in 5 minutes by arc#0005...                                                                            
+''')
+
+spam = input("[?] -> Which spam would you like to use?: ")
+
+wait(3)
+
+print("=> Starting spam...")
+
+pyautogui.moveTo(197, 307)
+while forever == 1:
+    pydirectinput.click()
+    pyautogui.typewrite(spam)
+    pydirectinput.press('enter')
